0e3b7c003bdd771471b08372eaeef5e0037f1785
Mark Skipper <marks@aptivate.org>
2017-02-24 22:31:09 +0000

    Develop tests for getPrevious

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,52 +6,90 @@ import Fuzz exposing (list, int, tuple, string)
 import String
 
 
+indexOf : (a -> Bool) -> List a -> Maybe Int
+indexOf predicate list =
+    list
+        |> List.indexedMap (,)
+        |> List.filter (\( i, val ) -> predicate val)
+        |> List.head
+        |> Maybe.map Tuple.first
+
+
 getPrevious : List a -> (a -> Bool) -> Maybe a
 getPrevious list predicate =
-    list |> List.length |> flip (-) 2 |> flip List.drop list |> List.head
+    let
+        decrement n =
+            if n > 0 then
+                Just (n - 1)
+            else
+                Nothing
+    in
+        list
+            |> indexOf predicate
+            |> Maybe.andThen decrement
+            |> Maybe.map (flip List.drop list)
+            |> Maybe.andThen List.head
 
 
 all : Test
 all =
-    describe "Test my get Previous function"
-        [ test "previous of last element is penultimate element" <|
+    describe "All tests"
+        [ testIndexOf
+        , testGetPrevious
+        ]
+
+
+testIndexOf : Test
+testIndexOf =
+    describe "Test my indexOf function"
+        [ test "Index of not found is nothing" <|
+            \() ->
+                indexOf ((==) 1) []
+                    |> Expect.equal Nothing
+        , test "Index of first item is Just 0" <|
+            \() ->
+                indexOf ((==) 1) [ 1 ]
+                    |> Expect.equal (Just 0)
+        , test "Index of second is 1" <|
+            \() ->
+                indexOf ((==) 2) [ 1, 2 ]
+                    |> Expect.equal (Just 1)
+        , test "Index of last is length -1" <|
             \() ->
                 let
                     ints =
                         [ 1, 2, 3 ]
-
-                    isThree =
-                        (==) 3
                 in
-                    Expect.equal (getPrevious ints isThree) (Just 2)
+                    indexOf ((==) 3) ints
+                        |> Expect.equal (Just <| List.length ints - 1)
+        , test "Index of fourth is 3" <|
+            \() ->
+                indexOf ((==) "d") [ "a", "b", "c", "d", "e", "f" ]
+                    |> Expect.equal (Just 3)
         ]
 
 
-old =
-    describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Addition" <|
-                \() ->
-                    Expect.equal (3 + 7) 10
-            , test "String.left" <|
-                \() ->
-                    Expect.equal "a" (String.left 1 "abcdefg")
-            , test "This test should fail - you should remove it" <|
-                \() ->
-                    Expect.fail "Failed as expected!"
-            ]
-        , describe "Fuzz test examples, using randomly generated input"
-            [ fuzz (list int) "Lists always have positive length" <|
-                \aList ->
-                    List.length aList |> Expect.atLeast 0
-            , fuzz (list int) "Sorting a list does not change its length" <|
-                \aList ->
-                    List.sort aList |> List.length |> Expect.equal (List.length aList)
-            , fuzzWith { runs = 1000 } int "List.member will find an integer in a list containing it" <|
-                \i ->
-                    List.member i [ i ] |> Expect.true "If you see this, List.member returned False!"
-            , fuzz2 string string "The length of a string equals the sum of its substrings' lengths" <|
-                \s1 s2 ->
-                    s1 ++ s2 |> String.length |> Expect.equal (String.length s1 + String.length s2)
-            ]
+testGetPrevious : Test
+testGetPrevious =
+    describe "Test my get Previous function"
+        [ test "previous of last element is penultimate element" <|
+            \() ->
+                getPrevious [ "a", "b", "c" ] ((==) "c")
+                    |> Expect.equal (Just "b")
+        , test "Previous of thrid element is second element" <|
+            \() ->
+                getPrevious [ 10, 20, 30, 40 ] ((==) 30)
+                    |> Expect.equal (Just 20)
+        , test "Previous of median element is element before" <|
+            \() ->
+                getPrevious [ 1, 2, 3 ] ((==) 2)
+                    |> Expect.equal (Just 1)
+        , test "no previous if item not found" <|
+            \() ->
+                getPrevious [] ((==) 3)
+                    |> Expect.equal Nothing
+        , test "no previous of first in list" <|
+            \() ->
+                getPrevious [ 1 ] ((==) 1)
+                    |> Expect.equal Nothing
         ]

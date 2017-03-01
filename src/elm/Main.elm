dee13075410ecff3ba92309bf92d8bba2afde2b5
Mark Skipper <marks@aptivate.org>
2017-03-01 15:31:59 +0000

    Fold findPage into location2messages

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -30,7 +30,7 @@ import WordpressRestApi as WP
 main =
     RouteUrl.program
         { delta2url = delta2hash
-        , location2messages = hash2message
+        , location2messages = location2messages
         , init = init
         , update = update
         , view = view
@@ -38,21 +38,21 @@ main =
         }
 
 
-hash2message : Location -> List Msg
-hash2message =
-    findPage >> Show >> List.singleton
+location2messages : Location -> List Msg
+location2messages =
+    Url.parseHash routeParser
+        >> Maybe.withDefault BadUrl
+        >> Show
+        >> List.singleton
 
 
 delta2hash : Model -> Model -> Maybe UrlChange
 delta2hash prevous current =
-    if prevous.page == current.page then
-        Nothing
-    else
-        current
-            |> currentRoute
-            |> toUrl
-            |> RouteUrl.UrlChange RouteUrl.NewEntry
-            |> Just
+    current
+        |> currentRoute
+        |> toUrl
+        |> RouteUrl.UrlChange RouteUrl.NewEntry
+        |> Just
 
 
 type alias Model =
@@ -121,20 +121,6 @@ currentRoute model =
                 |> Array.get index
                 |> Maybe.map (Blog << .slug)
                 |> Maybe.withDefault BadUrl
-
-
-{-| Under what circumstances should this return NotFound?
-  | - When the pattern doesn't match any of the routes,
-  | - Wheh the pattern is #blog/slug but slug can't be found
-  | Now when slug can't be found in the current cache we need to
-  | search for it and receive a negative answer before we know its
-  | not found.
--}
-findPage : Location -> Route
-findPage location =
-    location
-        |> Url.parseHash routeParser
-        |> Maybe.withDefault BadUrl
 
 
 routeParser : Url.Parser (Route -> Route) Route

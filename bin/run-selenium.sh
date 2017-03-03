4201b419fc5e4490fcf276675b720581468cf812
Mark Skipper <marks@aptivate.org>
2017-03-03 13:08:46 +0000

    Add geckodriver and scripts to run tests

---
+++ bin/run-selenium.sh
@@ -0,0 +1,2 @@
+java -Dwebdriver.gecko.driver="/home/mark/workspace/elm-wpc/bin/geckodriver" \
+  -jar selenium-server-standalone-3.1.0.jar 
---
+++ bin/runtests.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+TEST_SERVER="curl -s http://localhost:4444/wd/hub/status"
+
+GECKODRIVER="$DIR/bin/geckodriver"
+
+SELENIUM_JAR="$DIR/jar/selenium-server-standalone-3.1.0.jar"
+
+START_SERVER="java -Dwebdriver.gecko.driver=$GECKODRIVER -jar $SELENIUM_JAR"
+
+$TEST_SERVER > /dev/null || ($START_SERVER &)
+
+cd $DIR/webdriver-tests
+
+$DIR/node_modules/.bin/elm-webdriver

25b0fa0cd546e8bb881349a9b6ab317bb2a2542e
Mark Skipper <marks@aptivate.org>
2017-03-03 14:56:04 +0000

    Re-suffle web drvier tests

--- webdriver-tests/Tests.elm
+++ webdriver-tests/Tests.elm
@@ -18,40 +18,45 @@ baseUrl =
     "http://localhost:8000/"
 
 
-goDirectlyToEntry slug =
-    [ visit <| baseUrl ++ "#blog/" ++ slug ]
-
-
 testBlog : Run
 testBlog =
-    group "Tests of blog list"
-        [ describe "Base url redirects to blog with list of summaries"
-            [ visit baseUrl
-            , title <| Expect.equal "Bitterjug.com"
+    let
+        listExpectations =
+            [ title <| Expect.equal "Bitterjug.com"
             , url <| Expect.equal (baseUrl ++ "#blog")
             , elementCount "div.entry-summary" <| Expect.atLeast 10
             ]
-        , describe "Blog hash url leads to list of summaries"
-            [ visit <| baseUrl ++ "#blog"
-            , title <| Expect.equal "Bitterjug.com"
-            , url <| Expect.equal (baseUrl ++ "#blog")
-            , elementCount "div.entry-summary" <| Expect.atLeast 10
+    in
+        group "Tests of blog list"
+            [ describe "Base url redirects to blog with list of summaries" <|
+                [ visit baseUrl ]
+                    ++ listExpectations
+            , describe "Blog hash url leads to list of summaries" <|
+                [ visit <| baseUrl ++ "#blog" ]
+                    ++ listExpectations
             ]
-        , describe "Navigate from list to single entry"
-            [ visit <| baseUrl ++ "#blog"
-            , click ".entry-summary:nth-child(2)"
-            , url <| Expect.equal (baseUrl ++ "#blog/minimal-elm-program")
-            , elementCount "div.entry-summary" <| Expect.equal 1
-            ]
+
+
+navigateToEntryPage : Run
+navigateToEntryPage =
+    describe "Navigate from list to single entry"
+        [ visit <| baseUrl ++ "#blog"
+        , click ".entry-summary:nth-child(2)"
+        , url <| Expect.equal (baseUrl ++ "#blog/minimal-elm-program")
+        , elementCount "div.entry-summary" <| Expect.equal 1
         ]
 
 
 testEntry : Run
 testEntry =
-    group "Tests of blog single entries"
-        [ describe "Next of previous of next take you back to the same page" <|
-            goDirectlyToEntry "minimal-elm-program"
-                ++ [ url <| Expect.equal (baseUrl ++ "#blog/minimal-elm-program")
-                   , elementCount "div.entry-summary" <| Expect.equal 1
-                   ]
-        ]
+    let
+        goDirectlyToEntry slug =
+            [ visit <| baseUrl ++ "#blog/" ++ slug ]
+    in
+        group "Tests of blog single entries"
+            [ describe "Next of previous of next take you back to the same page" <|
+                goDirectlyToEntry "minimal-elm-program"
+                    ++ [ url <| Expect.equal (baseUrl ++ "#blog/minimal-elm-program")
+                       , elementCount "div.entry-summary" <| Expect.equal 1
+                       ]
+            ]
